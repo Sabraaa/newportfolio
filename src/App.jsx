85980c8f6a04048a3417efeeeb378db8bcb74b6a
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-10-17 19:55:46 +0330

    navbar responsive and style change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import Header from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-
   return (
     <>
-<Header/>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import "./Navbar.css";
+import Logo from "../../assets/Logo.png";
+import { IoLogoGithub, IoLogoLinkedin, IoMdMail } from "react-icons/io";
+
+const Navbar = () => {
+  return (
+    <div className="section__padding ">
+      <div className="my__nav" id="home">
+        <div className="my__nav-logo">
+          <img src={Logo} alt="logo" />
+        </div>
+
+        <div className="my__nav-menu">
+          <p>
+            <a href="#projects"> Projects</a>
+          </p>
+          <p>
+            <a href="#technologies">Technologies</a>
+          </p>
+          <p>
+            <a href="#aboutme">About me</a>
+          </p>
+        </div>
+
+        <div className="my__nav-icon">
+          <IoLogoLinkedin size={22} />
+          <IoLogoGithub size={22} />
+          <IoMdMail size={22} />
+        </div>
+      </div>
+      <div className="my__nav-mediamenu">
+        <p>
+          <a href="#projects"> Projects</a>
+        </p>
+        <p>
+          <a href="#technologies">Technologies</a>
+        </p>
+        <p>
+          <a href="#aboutme">About me</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
