5100edc3934684f3a2712481e46422e4965049b7
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-10-17 20:21:37 +0330

    buttun half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import GradientButton from "./components/GradientButton";
 
 function App() {
   return (
     <>
       <Navbar />
+      <GradientButton/>
     </>
   );
 }
---
+++ src/components/GradientButton.jsx
@@ -0,0 +1,12 @@
+import "./GradientButton.css"
+const GradientButton = () => {
+  return (
+    <div className="gradientbutton">
+        <button className="gradient__bg">
+        salaaaaaaaaaaaaaaaaam
+        </button>
+    </div>
+  )
+}
+
+export default GradientButton
