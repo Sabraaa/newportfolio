b30ca2c95bf7ddbb28ea5568502e93fd73a5e83e
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-10-21 19:11:12 +0330

    paddings fixed in header and projects

--- src/container/projects/Projects.jsx
+++ src/container/projects/Projects.jsx
@@ -3,9 +3,9 @@ import "./Projects.css"
 
 const Projects = () => {
   return (
-    <div className="my__projects section__padding ">
+    <div className="my__projects section__padding"  id="projects">
     <h1> Projects</h1>
-    <div className="my__projects-wrapper" id="#projects">    
+    <div className="my__projects-wrapper">    
       <ProjectCard title="projectname" description="tecnologies i used" />
       <ProjectCard title="projectname" description="tecnologies i used" />
       <ProjectCard title="projectname" description="tecnologies i used" />
