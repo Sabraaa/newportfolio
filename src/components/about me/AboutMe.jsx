f8c52a6c0210a57af9205c53286492dec6807d27
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-10-28 17:54:13 +0330

    same bullet styling with pseudo element

--- src/components/about me/AboutMe.jsx
+++ src/components/about me/AboutMe.jsx
@@ -5,11 +5,11 @@ const AboutMe = () => {
     <div id="aboutme" className="my__aboutme section__padding">
       <h1>About me</h1>
       <div className="my__aboutme-line"></div>
-{/* 
+
       <div className="my__aboutme-points">
         <div className="my__aboutme-points_each">
           <h3>2020</h3>
-          <Bullet />
+          <div className="same__bullet"> <div/>
           <p>
             I embarked on my coding journey and gained hands-on experience as a
             frontend React intern at <span>Pejvak</span>. During this immersive
@@ -20,7 +20,7 @@ const AboutMe = () => {
 
         <div className="my__aboutme-points_each">
           <h3>2021</h3>
-          <Bullet />
+          <div className="same__bullet"> <div/
           <p>
             After concluding my internship, I transitioned into freelance work,
             taking on projects solo. I skillfully developed dashboards,
@@ -31,7 +31,7 @@ const AboutMe = () => {
 
         <div className="my__aboutme-points_each">
           <h3>2022</h3>
-          <Bullet />
+          <div className="same__bullet"> <div/
           <p>
             At <span>SiteNova</span>, I honed essential team skills and advanced
             frontend proficiency. This experience empowered me to excel in
@@ -41,14 +41,14 @@ const AboutMe = () => {
 
         <div className="my__aboutme-points_each">
           <h3>2023</h3>
-          <Bullet />
+          <div className="same__bullet"> <div/
           <p>
             Now, armed with a polished portfolio, I'm ready to tackle corporate
             challenges. I'm motivated to excel in professional roles while
             keeping my passion for ongoing learning in the coding world.
           </p>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
