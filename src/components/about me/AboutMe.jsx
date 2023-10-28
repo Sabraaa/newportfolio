cd18619df5770a93ee2e9590373839b10209713c
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-10-28 18:35:06 +0330

    line about me before mostly done

--- src/components/about me/AboutMe.jsx
+++ src/components/about me/AboutMe.jsx
@@ -7,9 +7,10 @@ const AboutMe = () => {
       <div className="my__aboutme-line"></div>
 
       <div className="my__aboutme-points">
+        {/* STEP 1 */}
         <div className="my__aboutme-points_each">
           <h3>2020</h3>
-          <div className="same__bullet"> <div/>
+          <div className="same__bullet"></div>
           <p>
             I embarked on my coding journey and gained hands-on experience as a
             frontend React intern at <span>Pejvak</span>. During this immersive
@@ -17,10 +18,10 @@ const AboutMe = () => {
             ability to tackle a wide range of tasks.
           </p>
         </div>
-
+        {/* STEP 2 */}
         <div className="my__aboutme-points_each">
           <h3>2021</h3>
-          <div className="same__bullet"> <div/
+          <div className="same__bullet"></div>
           <p>
             After concluding my internship, I transitioned into freelance work,
             taking on projects solo. I skillfully developed dashboards,
@@ -28,20 +29,20 @@ const AboutMe = () => {
             knowledge base.
           </p>
         </div>
-
+        {/* STEP 3 */}
         <div className="my__aboutme-points_each">
           <h3>2022</h3>
-          <div className="same__bullet"> <div/
+          <div className="same__bullet"></div>
           <p>
             At <span>SiteNova</span>, I honed essential team skills and advanced
             frontend proficiency. This experience empowered me to excel in
             collaborative environments while elevating my technical expertise.
           </p>
         </div>
-
+        {/* STEP 4 */}
         <div className="my__aboutme-points_each">
           <h3>2023</h3>
-          <div className="same__bullet"> <div/
+          <div className="same__bullet"></div>
           <p>
             Now, armed with a polished portfolio, I'm ready to tackle corporate
             challenges. I'm motivated to excel in professional roles while
