c398d16bcde10a89a1438271419cb862c96e2d3c
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-11-05 19:15:29 +0330

    fix responsive b ugs in technologies an projects

--- src/container/projects/Projects.jsx
+++ src/container/projects/Projects.jsx
@@ -14,28 +14,28 @@ const Projects = () => {
       <div className="my__projects-wrapper">
         <ProjectCard
           title="Sabras GPT-3"
-          description="Figma-based, fully responsive project about GPT-3, created using React, Js, CSS, and BEM methodology"
+          description="Figma-based, responsive, created using React, Js, CSS, and BEM methodology."
           image={SC1}
           link="https://sabrasgpt.vercel.app/"
           github="https://github.com/Sabraaa/sabrasgpt"
         />
         <ProjectCard
           title="My Portfolio"
-          description="Figma-based, fully responsive portfolio project, created using React+Vite, Js, CSS, and BEM methodology"
+          description="Figma-based, responsive, created using React+Vite, Js, CSS, and BEM methodology."
           image={SC2}
           link="https://newportfolio-hazel-eta.vercel.app/"
           github="https://github.com/Sabraaa/newportfolio"
         />
         <ProjectCard
           title="The Dashboard"
-          description="A wallet with dashboard project, created using React, Js, Shopify, CSS and SASS"
+          description="A wallet with dashboard project, created using React, Js, Shopify, CSS and SASS."
           image={SC3}
           link="https://luxury-gecko-4f43a0.netlify.app/"
           github="https://github.com/Sabraaa/sabrasdashboard"
         />
         <ProjectCard
           title="Sabra's Portfolio"
-          description="Minimal portfolio project, created using React, Js, CSS and SASS"
+          description="Minimal portfolio project, created using React, Js, CSS and SASS."
           image={SC4}
           link="https://sabrasportfilio.vercel.app/"
           github="https://github.com/Sabraaa/sabrasportfilio"
