1febbdcdb0fef69fe01257641718c93b1fc5c755
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-11-11 17:57:42 +0330

    dealing with the components

--- src/components/verticalStepper/VerticalStepper.jsx
+++ src/components/verticalStepper/VerticalStepper.jsx
@@ -14,8 +14,8 @@ const VerticalStepper = () => {
           contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
           contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
           date="2011 - present"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<WorkIcon />}
+          iconStyle={{ background: "linear-gradient(228deg, rgba(124, 221, 185, 1) 0%, rgba(114, 189, 238, 1) 34%, rgba(169, 122, 215, 1) 100%)", color: "#fff" }}
+
         >
           <h3 className="vertical-timeline-element-title">Creative Director</h3>
           <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
@@ -28,7 +28,6 @@ const VerticalStepper = () => {
           className="vertical-timeline-element--work"
           date="2010 - 2011"
           iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">Art Director</h3>
           <h4 className="vertical-timeline-element-subtitle">
@@ -43,7 +42,6 @@ const VerticalStepper = () => {
           className="vertical-timeline-element--work"
           date="2008 - 2010"
           iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">Web Designer</h3>
           <h4 className="vertical-timeline-element-subtitle">
@@ -55,7 +53,6 @@ const VerticalStepper = () => {
           className="vertical-timeline-element--work"
           date="2006 - 2008"
           iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">Web Designer</h3>
           <h4 className="vertical-timeline-element-subtitle">
