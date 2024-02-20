95e10014ae33a8eca59f1709265b7b4bae8196d0
Sabra <100771060+Sabraaa@users.noreply.github.com>
2024-02-20 17:19:00 +0330

    technologies edited

--- src/container/technologies/Technologies.jsx
+++ src/container/technologies/Technologies.jsx
@@ -1,17 +1,16 @@
 import Bullet from "../../components/bullet/Bullet";
-import ProgressBar from "../../components/ProgressBar/ProgressBar"
+import ProgressBar from "../../components/ProgressBar/ProgressBar";
 import "./Technologies.css";
 
 const Technologies = () => {
   return (
     <div>
       <div className="my__technologies section__margin" id="technologies">
-      
         <h1> Technologies</h1>
         <div className="my__technologies-progress__wrapper">
           <ProgressBar title="HTML" />
           <ProgressBar title="CSS3, Tailwind, Sass, Less, Bootstrap" />
-          <ProgressBar title="Javascript" />
+          <ProgressBar title="Javascript, Typescript" />
           <ProgressBar title="React, Routings, Redux" />
           <ProgressBar title="Git, GitHub" />
           <ProgressBar title="Netlify, Vercel" />
@@ -19,19 +18,19 @@ const Technologies = () => {
 
         <h1> Soft Skills</h1>
         <div className="my__technologies-bullet__wrapper">
-          <Bullet text="Communication Skills"/>
-          <Bullet text="Teamwork"/>
-          <Bullet text="Attention to Detail"/>
-          <Bullet text="Problem-Solving "/>
-          <Bullet text="Time Management"/>
-          <Bullet text="Adaptability"/>
-          <Bullet text="C1 English"/>
-          <Bullet text="B1 German"/>
+          <Bullet text="Communication Skills" />
+          <Bullet text="Teamwork" />
+          <Bullet text="Attention to Detail" />
+          <Bullet text="Problem-Solving " />
+          <Bullet text="Time Management" />
+          <Bullet text="Adaptability" />
+          <Bullet text="C1 English" />
+          <Bullet text="B1 German" />
         </div>
         <h1> Additional Skills</h1>
         <div className="my__technologies-additional__skills">
-          <Bullet text="Basic Knowledge of SEO Principles"/>
-          <Bullet text="Adobe Tools: ps, Ai, Lr, Pr"/>
+          <Bullet text="Basic Knowledge of SEO Principles" />
+          <Bullet text="Adobe Tools: ps, Ai, Lr, Pr" />
         </div>
       </div>
     </div>
